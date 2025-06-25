e75db6c5faae1b002c66150e09b90e75d0d90b59
Juma de Araújo Braga <jumabraga_13@hotmail.com>
2025-06-25 09:42:04 -0300

    Add files via upload

--- super_trunfo.c
+++ super_trunfo.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
 
-int main() {
-    int pontostur1, populacao1;         // Dados CARTA 1
+int main(){
+    int pontostur1, populacao1;
     char codigo1[30], cidade1[30];
     char estado1;
-    float pib1, area1, densidpop1 = 0, pibpercap1 = 0;
+    float pib1, area1;
 
-    int pontostur2, populacao2;          // Dados CARTA 1
+    int  pontostur2, populacao2;
     char codigo2[30], cidade2[30];
     char estado2;
-    float pib2, area2, densidpop2 = 0, pibpercap2 = 0;
+    float pib2, area2;
+    
+        /* Dados Referente a primeira e segunda carta: Estado, 
+        Código, Nome da Cidade, População, Área: em km², PIB 
+        em bilhões de reais, Número de Pontos Turísticos.*/
 
-    // Associando as informações que serão solicitadas ao usuário CARTA 1
-    printf("Preencha os dados do Super Trunfo: \nCarta 1:\n");
+    printf("Preencha os dados do Super Trunfo: \nCarta: 1\n");
 
     printf("Digite o Estado: ");
     scanf("%s", &estado1);
@@ -35,9 +38,8 @@ int main() {
     printf("Digite o Número de Pontos Turísticos: ");
     scanf("%d", &pontostur1);
 
-    // Associando as informações que serão solicitadas ao usuário CARTA 2
-    
-    printf("Preencha os dados do Super Trunfo: \nCarta 2:\n");
+
+    printf("Preencha os dados do Super Trunfo: \nCarta: 2\n");
 
     printf("Digite o Estado: ");
     scanf("%s", &estado2);
@@ -59,16 +61,13 @@ int main() {
 
     printf("Digite o Número de Pontos Turísticos: ");
     scanf("%d", &pontostur2);
+    
+        /*Associando as informações que serão solicitadas ao usuário 
+        e as respectivas referências dentro do programa, (dados de
+        entrada e saída)*/
 
-    // Densidade - Evitando divisão por zero
-    densidpop1 = (populacao1 / area1) * (area1 != 0);
-    densidpop2 = (populacao2 / area2) * (area2 != 0);
 
-    // PIB per capita: PIB em bilhões * 1e9 / população
-    pibpercap1 = ((pib1 * 1e9) / populacao1) * (populacao1 != 0);
-    pibpercap2 = ((pib2 * 1e9) / populacao2) * (populacao2 != 0);
-
-     /* Exibindo os dados coletados */
+        /* Exibindo os dados coletados */
     printf("\n--- Resultado ---\n");
     printf("\nCarta: 1\n");
     printf("Estado: %c\n", estado1);
@@ -78,8 +77,6 @@ int main() {
     printf("Área: %.2f km²\n", area1);
     printf("PIB: R$ %.2f bilhões\n", pib1);
     printf("Número de Pontos Turísticos: %d\n", pontostur1);
-    printf("Densidade Populacional: %.2f hab/km²\n", densidpop1);
-    printf("PIB per Capita: R$ %.2f\n", pibpercap1);
 
     printf("\nCarta: 2\n");
     printf("Estado: %c\n", estado2);
@@ -89,8 +86,8 @@ int main() {
     printf("Área: %.2f km²\n", area2);
     printf("PIB: R$ %.2f bilhões\n", pib2);
     printf("Número de Pontos Turísticos: %d\n", pontostur2);
-    printf("Densidade Populacional: %.2f hab/km²\n", densidpop2);
-    printf("PIB per Capita: R$ %.2f\n", pibpercap2);
+
 
     return 0;
+
 }
